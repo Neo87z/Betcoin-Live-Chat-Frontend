3e4268bcab2f7bf81e2e87fba2b7a5de7d22f26b
Neo87z <shehanhoradagoda12@gmail.com>
2022-09-25 09:28:38 -0230

    'main;

--- src/App.jsx
+++ src/App.jsx
@@ -46,16 +46,7 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog-post" element={<BlogPost />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
