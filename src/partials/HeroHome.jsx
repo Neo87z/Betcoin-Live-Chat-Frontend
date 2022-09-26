4aedb50b5aa42174404771556c48013b318d8b3a
Neo87z <shehanhoradagoda12@gmail.com>
2022-09-26 11:53:00 -0230

    'main'

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -8,7 +8,7 @@ function HeroHome() {
   const [videoModalOpen, setVideoModalOpen] = useState(false);
 
   return (
-    <section style={{marginTop:'-120px',background:'black'}}>
+    <section style={{marginTop:'-120px'}}>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
 
         {/* Illustration behind hero content */}
